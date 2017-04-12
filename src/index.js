0cbfb188d95173519e6cdc9d708389f9fa996456
Luis Diaz <diazluis1048@gmail.com>
2017-04-12 11:02:32 -0400

    second commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyD8cnEFffTqzazPPy0sACG5waN_FsJnqH4';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+//Create new component that produces some html
+const App = () => {//const is an ES6 piece of syntax.  Declares a variable who's value never changes
+  return (
+    <div>
+      <SearchBar />
+    </div>//JSX (a subset of JS that allows us to write JS that looks like HTML)
+  );
+}//fat arrow is equivalent to writing function
+
+//Take this component's generated HTML and put it on the page (in DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
