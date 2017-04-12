cac98874bc2bbdee1ddf8820524d49afe688df06
Luis Diaz <diazluis1048@gmail.com>
2017-04-12 15:07:20 -0400

    refactored functional components to class components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyD8cnEFffTqzazPPy0sACG5waN_FsJnqH4';
 
-//Create new component that produces some html
-const App = () => {//const is an ES6 piece of syntax.  Declares a variable who's value never changes
-  return (
-    <div>
-      <SearchBar />
-    </div>//JSX (a subset of JS that allows us to write JS that looks like HTML)
-  );
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data){
+  console.log(data);
+});
+
+class App extends Component {//const is an ES6 piece of syntax.  Declares a variable who's value never changes
+  render(){
+    return (
+      <div>
+        <SearchBar />
+      </div>//JSX (a subset of JS that allows us to write JS that looks like HTML)
+    );
+  }
 }//fat arrow is equivalent to writing function
 
 //Take this component's generated HTML and put it on the page (in DOM)
