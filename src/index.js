88f629ec990d3cfbdfae7bc15b64d905c670a87b
Luis Diaz <diazluis1048@gmail.com>
2017-04-12 18:37:21 -0400

    added functionality that allows users to search for videos of their choice

--- src/index.js
+++ src/index.js
@@ -15,18 +15,21 @@ class App extends Component {
        selectedVideo: null
     };
 
-    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {//fat arrow is equivalent to writing function
+    this.videoSearch('surfboards');
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {//fat arrow is equivalent to writing function
       this.setState({
          videos: videos,
          selectedVideo: videos[0]
        });
     });
   }
-
   render(){
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange = {term => this.videoSearch(term)}/>
         <VideoDetail video = {this.state.selectedVideo}/>
         <VideoList
           onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
