a15238c203f334a1f7f3dd3ef4a8f8c8f35581b4
Luis Diaz <diazluis1048@gmail.com>
2017-06-14 16:51:56 -0400

    pls

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -4,12 +4,12 @@ const VideoDetail = ({video}) => {
   if (!video) {
     return <div>Loading...</div>;
   }
-  const videoId = video.id.VideoId;
+  const videoId = video.id.videoId;
   const url = `https://www.youtube.com/embed/${videoId}`;
   return (
     <div className = "video-detail col-md-8">
       <div className = "embed-responsive embed-responsive-16by9">
-        <iframe className = "embed-responsive-item" src = {url}></iframe>
+        <iframe className = "embed-responsive-item" src={url}></iframe>
       </div>
       <div className = "details">
         <div>{video.snippet.title}</div>
